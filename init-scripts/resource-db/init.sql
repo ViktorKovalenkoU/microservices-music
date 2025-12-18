4e6eb242a4ee8ec5a45bb030dcacab40c5b667e8
Viktor <javatrainin1337@gmail.com>
2025-12-18 19:25:12 +0200

    task4 fix

--- init-scripts/resource-db/init.sql
+++ init-scripts/resource-db/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS resources (
     id BIGSERIAL PRIMARY KEY,
-    data BYTEA NOT NULL,
+    storage_key VARCHAR(255) NOT NULL UNIQUE,
     content_type VARCHAR(255),
     file_name VARCHAR(255)
 );
